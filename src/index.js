efe85d9ec9d945908025fa1689c52a795d614601
NicMortelliti <npmortelliti@gmail.com>
2022-02-26 15:22:47 -0800

    commit tree e27d89dbb4499121f98cbf738ed0cde4316f5a17

--- src/index.js
+++ src/index.js
@@ -1,5 +1,41 @@
 const init = () => {
-  
-}
+  // Target form
+  const inputForm = document.querySelector("form");
 
-document.addEventListener('DOMContentLoaded', init);
+  // Listen for submit
+  inputForm.addEventListener("submit", event => {
+    // Prevent page reload
+    event.preventDefault();
+
+    // Access user-entered data 1 of 2 ways...
+    // 1) Via the returned event data:
+    //console.log(event.target.children[1].value);
+
+    // 2) Via direct access to the element:
+    const input = document.querySelector("input#searchByID");
+    console.log(input.value);
+
+    /*
+     Fetch data specifically related to the
+     movie entered by the user by tacking on
+     ${input} to the fetch url.
+     */
+    fetch(`http://localhost:3000/movies/${input.value}`)
+      .then(response => response.json())
+      .then(data => {
+        const title = document.querySelector("section#movieDetails h4");
+        const summary = document.querySelector("section#movieDetails p");
+
+        // I added a check for invalid form entries here
+        if (typeof data.title in window) {
+          data.title = "Please enter a valid movie ID";
+          data.summary = "";
+        }
+
+        title.innerText = data.title;
+        summary.innerText = data.summary;
+      });
+  });
+};
+
+document.addEventListener("DOMContentLoaded", init);
